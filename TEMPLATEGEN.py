2048390d8c132289bc7342fc344cf3c5dceec461
Eryn <32990348+StrawberryFields0@users.noreply.github.com>
2025-11-10 14:28:25 -0500

    Added more stuff

--- TEMPLATEGEN.py
+++ TEMPLATEGEN.py
@@ -1,6 +1,3 @@
-
-
-
 company = input("Company Name")
 unit = input("Enter your unit #")
 age = input("How old is the Paitent")
@@ -13,8 +10,25 @@ GCSM = input("Gcs Motor Score")
 GCSV = input("GCS Verbal Score")
 staff_reason = input("Staff reason for transport")
 CMN = input("What does CMN state")
-numvitals
-
-
-
+PMHX = input("enter pt PMHX")
+PTposition = input("what position was paitent in")
+transportnotes = input("enter transport notes")
+othernotes= input("Input other notes")
+pinksliped = input("are they pink slipped y/n").lower()
+    if pinksliped = "y"
+        pinkslip = input("state what the Pinkslip says")
+    else  pinkslip = "N/A"
+Medsgivenenroute = input("Did you  initiate or continue any meds y/n").lower()
+    if Medsgivenenroute = "y" 
+        initRXTX = input("what meds?")
+    else initRXTX = "none given"
+Hospitalgivenmeds = input("did hospital give any meds y/n").lower()
+    if Hospitalgivenmeds = "y"
+        hospitalrx = input("Input hospital meds")
+    else:
+         hospitalrx ="None given"
 gcs_total = GCSE + GCSM + GCSV
+numvitals
+    
+
+
